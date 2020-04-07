fea7e75a3ff7b390e1038829063b9394183bc27b
Jeep <jeep@Mac.fritz.box>
2020-04-07 20:17:18 +0200

    determining seasons

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+    state = { lat: null, errMessage: ''};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    componentDidMount () {
+        window.navigator.geolocation.getCurrentPosition(
+        //To update state object, we call setState. setState is automatically put on our app component when we extended React.Component
+        //Anytime you want to update your state, you have to call setState
+            position => this.setState ({ lat: position.coords.latitude }),
+            err => this.setState ({ errMessage: err.message})
+        //Single line code, no curry bracket nor semi-colon
+            );
+    }
+
+
+    //React says we have to define render!! 
+    render() {
+        if(this.state.errMessage && !this.state.lat) {
+            return <div>Error: {this.state.errMessage}</div>
+        }
+
+        if(!this.state.errMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} /> 
+            //Taking the property from the states on the app component and passing it as a prop down into the SeasonDisplay
+        }
+
+        return <div>Loading... </div>
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
